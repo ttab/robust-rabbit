53d9d0b76f7b15b1f3b44d89dbf5aa3345fc6924
Fredrik Appelberg <fredrik.appelberg@tt.se>
2020-09-22 15:59:52 +0200

    I am weary, let me rest

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
         ".(ts|tsx)": "ts-jest",
     },
     testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-    testPathIgnorePatterns: ['globals.js'],
+    testPathIgnorePatterns: ['globals.ts'],
     moduleFileExtensions: ["ts", "tsx", "js", "json"],
-    setupFilesAfterEnv: ['jest-sinon', '<rootDir>/__tests__/globals.js']
+    setupFilesAfterEnv: ['jest-sinon', '<rootDir>/__tests__/globals.ts']
 };
