f1e6f0e5971fc13953fe1c368428d8cc69fc0e0c
Fredrik Appelberg <fredrik.appelberg@tt.se>
2023-10-17 13:57:19 +0200

    Added tests for emitted events

--- __tests__/ack-test.ts
+++ __tests__/ack-test.ts
@@ -6,13 +6,9 @@ import { expect } from 'chai'
 import { Ack } from "../src/ack";
 import { TqEvents } from "../src/tenacious-q";
 
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 describe("Ack", function() {
-  let _ack, ack, headers, info, msg, exchange, events;
+  let _ack, ack, headers, info, msg, exchange
+  let events: TypedEventEmitter<TqEvents<unknown>>;
 
   beforeEach(function() {
     exchange = { publish: stub().returns(Promise.resolve()) };
@@ -24,8 +20,8 @@ describe("Ack", function() {
     ack = new Ack(exchange, msg, headers, info, events, _ack);
   });
 
-  describe("._mkopts()", function() {
-    it("should copy relevant headers from deliveryInfo", function() {
+  describe("._mkopts()", () => {
+    it("should copy relevant headers from deliveryInfo", () => {
       const opts = ack._mkopts(
         {},
         {
@@ -41,7 +37,7 @@ describe("Ack", function() {
       opts.should.not.have.property("myHeader");
     });
 
-    it("should copy existing headers, and add a retryCount", function() {
+    it("should copy existing headers, and add a retryCount", () => {
       const opts = ack._mkopts({ panda: "cub" }, {}, 23);
       opts.should.have.property("headers");
       opts.headers.should.eql({
@@ -50,7 +46,7 @@ describe("Ack", function() {
       });
     });
 
-    it("should copy the original routingKey", function() {
+    it("should copy the original routingKey", () => {
       const opts = ack._mkopts(
         {},
         {
@@ -61,7 +57,7 @@ describe("Ack", function() {
       opts.headers.should.have.property("tq-routing-key", "panda");
     });
 
-    it("should set the retryCount and routingKey even if there are no existing headers", function() {
+    it("should set the retryCount and routingKey even if there are no existing headers", () => {
       const opts = ack._mkopts(undefined, { routingKey: "panda" }, 23);
       opts.should.have.property("headers");
       opts.headers.should.eql({
@@ -71,13 +67,14 @@ describe("Ack", function() {
     });
   });
 
-  describe("._msgbody()", function() {
-    it("should return the msg if this it a plain javascript object", function() {
+  describe("._msgbody()", () => {
+
+    it("should return the msg if this it a plain javascript object", () => {
       msg = { name: "panda" };
       ack._msgbody(msg, "application/json").should.eql(msg);
     });
 
-    it("should return the data part if it exists and is a Buffer", function() {
+    it("should return the data part if it exists and is a Buffer", () => {
       const body = Buffer.from("panda");
       msg = {
         data: body,
@@ -88,111 +85,122 @@ describe("Ack", function() {
   });
 
   describe(".acknowledge()", () => {
-    it("should call the underlying acknowledge() fn at most once", () =>
-      Promise.all([ack.acknowledge(), ack.acknowledge()]).then(
-        () => _ack.acknowledge.should.have.been.calledOnce
-      ));
+
+    it("should call the underlying acknowledge() fn at most once", async () => {
+      await Promise.all([ack.acknowledge(), ack.acknowledge()]);
+      _ack.acknowledge.should.have.been.calledOnce;
+    });
+
+    it('should emit a acknowledged event', (done) => {
+      events.on('acknowledged', () => done())
+      ack.acknowledge()
+    })
+
   });
 
-  describe(".retry()", function() {
-    it("should queue the message for retry if allowed by retry count", function() {
+  describe(".retry()", () => {
+
+    it("should queue the message for retry if allowed by retry count", async () => {
       headers["tq-retry-count"] = 1;
-      return ack.retry().then(function(v) {
-        exchange.publish.should.have.been.calledWith(
-          "retry",
-          msg,
-          match({
-            contentType: "application/json",
-            headers: { "tq-retry-count": 2 },
-          })
-        );
-        _ack.acknowledge.should.have.been.calledOnce;
-        return v.should.eql(2);
-      });
-    });
-
-    it("should queue the message as a failure if we have reached max number of retries", function() {
-      headers["tq-retry-count"] = 3;
-      return ack.retry().then(function(v) {
-        exchange.publish.should.have.been.calledWith(
-          "fail",
-          msg,
-          match({
-            contentType: "application/json",
-            headers: { "tq-retry-count": 4 },
-          })
-        );
-        _ack.acknowledge.should.have.been.calledOnce;
-        return v.should.eql(0);
-      });
-    });
-
-    it("should not retry if acknowledge() has already been called", () =>
-      ack.acknowledge().then(() =>
-        ack.retry().then(function(v) {
-          _ack.acknowledge.should.have.been.calledOnce;
-          exchange.publish.should.not.have.been.called;
-          return expect(v).to.be.undefined;
-        })
-      ));
-
-    it("should not fail if acknowledge() has already been called", function() {
-      headers["tq-retry-count"] = 3;
-      return ack.acknowledge().then(() =>
-        ack.retry().then(function(v) {
-          exchange.publish.should.not.have.been.called;
-          return expect(v).to.be.undefined;
+      const v = await ack.retry()
+      exchange.publish.should.have.been.calledWith(
+        "retry",
+        msg,
+        match({
+          contentType: "application/json",
+          headers: { "tq-retry-count": 2 },
         })
       );
+      _ack.acknowledge.should.have.been.calledOnce;
+      v.should.eql(2);
     });
 
-    it("should not be possible to retry many times in a row", () =>
-      Promise.all([
+    it("should queue the message as a failure if we have reached max number of retries", async () => {
+      headers["tq-retry-count"] = 3;
+      const v = await ack.retry()
+      exchange.publish.should.have.been.calledWith(
+        "fail",
+        msg,
+        match({
+          contentType: "application/json",
+          headers: { "tq-retry-count": 4 },
+        })
+      );
+      _ack.acknowledge.should.have.been.calledOnce;
+      v.should.eql(0);
+    });
+
+    it("should not retry if acknowledge() has already been called", async () => {
+      await ack.acknowledge()
+      const v = await ack.retry()
+      _ack.acknowledge.should.have.been.calledOnce;
+      exchange.publish.should.not.have.been.called;
+      expect(v).to.be.undefined;
+    });
+
+    it("should not fail if acknowledge() has already been called", async () => {
+      headers["tq-retry-count"] = 3;
+      await ack.acknowledge()
+      const v = await ack.retry()
+      exchange.publish.should.not.have.been.called;
+      return expect(v).to.be.undefined;
+    });
+
+    it("should not be possible to retry many times in a row", async () => {
+      await Promise.all([
         ack.retry(),
         ack.retry(),
         ack.retry(),
         ack.retry(),
         ack.retry(),
-      ]).then(function() {
-        _ack.acknowledge.should.have.been.calledOnce;
-        return exchange.publish.should.have.been.calledOnce;
-      }));
+      ]);
+      _ack.acknowledge.should.have.been.calledOnce;
+      exchange.publish.should.have.been.calledOnce;
+    });
+
+    it('should emit a retried event', (done) => {
+      events.on('retried', () => done());
+      ack.retry();
+    });
   });
 
-  describe(".fail()", function() {
-    it("should queue the message as a failure", () =>
-      ack.fail().then(function(v) {
-        exchange.publish.should.have.been.calledWith(
-          "fail",
-          msg,
-          match({
-            contentType: "application/json",
-            headers: { "tq-retry-count": 0 },
-          })
-        );
-        _ack.acknowledge.should.have.been.calledOnce;
-        return v.should.eql(0);
-      }));
-
-    it("should do nothing if acknowledge() has already been called", () =>
-      ack.acknowledge().then(() =>
-        ack.fail().then(function(v) {
-          _ack.acknowledge.should.have.been.calledOnce;
-          exchange.publish.should.not.have.been.called;
-          return expect(v).to.be.undefined;
+  describe(".fail()", () => {
+    it("should queue the message as a failure", async () => {
+      const v = await ack.fail()
+      exchange.publish.should.have.been.calledWith(
+        "fail",
+        msg,
+        match({
+          contentType: "application/json",
+          headers: { "tq-retry-count": 0 },
         })
-      ));
+      );
+      _ack.acknowledge.should.have.been.calledOnce;
+      v.should.eql(0);
+    });
 
-    it("should not be possible to fail many times in a row", () =>
-      Promise.all([
+    it("should do nothing if acknowledge() has already been called", () => ack.acknowledge().then(() => ack.fail().then(function(v) {
+      _ack.acknowledge.should.have.been.calledOnce;
+      exchange.publish.should.not.have.been.called;
+      expect(v).to.be.undefined;
+    })
+    ));
+
+    it("should not be possible to fail many times in a row", async () => {
+      await Promise.all([
         ack.fail(),
         ack.fail(),
         ack.fail(),
         ack.fail(),
         ack.fail(),
-      ]).then(function() {
-        _ack.acknowledge.should.have.been.calledOnce;
-        return exchange.publish.should.have.been.calledOnce;
-      }));
+      ]);
+      _ack.acknowledge.should.have.been.calledOnce;
+      exchange.publish.should.have.been.calledOnce;
+    });
+
+    it('should emit a retried event', (done) => {
+      events.on('retried', () => done());
+      ack.retry();
+    });
   });
 });
--- __tests__/tenacious-q-test.ts
+++ __tests__/tenacious-q-test.ts
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import { TenaciousQ, TqCallback } from "../src/tenacious-q";
 import { stub, spy, match, SinonStub } from "sinon";
 import { expect } from "chai";
@@ -50,7 +45,11 @@ describe("TenaciousQ", function() {
       msg = {};
       headers = {};
       info = {};
-      return (ack = { acknowledge: spy(), retry: spy(), fail: spy() });
+      ack = {
+        acknowledge: spy(),
+        retry: spy(),
+        fail: spy()
+      };
     });
 
     it("should invoke the listener", () =>
@@ -96,104 +95,99 @@ describe("TenaciousQ", function() {
       tq._listen(listener, msg, headers, info, ack).then(function() {
         ack.acknowledge.should.have.been.calledOnce;
         ack.retry.should.not.have.been.called;
-        return ack.fail.should.not.have.been.called;
+        ack.fail.should.not.have.been.called;
       }));
 
-    it("should call ack.retry() on failure", function() {
+    it("should call ack.retry() on failure", (done) => {
       listener.throws(new Error("such fail!"));
-      return tq._listen(listener, msg, headers, info, ack).then(function() {
-        ack.retry.should.have.been.calledOnce;
-        ack.acknowledge.should.not.have.been.called;
-        return ack.fail.should.not.have.been.called;
-      });
+      tq.on('error', () => {
+        try {
+          ack.retry.should.have.been.calledOnce;
+          ack.acknowledge.should.not.have.been.called;
+          ack.fail.should.not.have.been.called;
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+      tq._listen(listener, msg, headers, info, ack)
     });
 
-    describe("when listener doesnt return a promise", function() {
-      it("should damn well NOT ack.acknowledge", function() {
+    describe("when listener doesnt return a promise", () => {
+
+      it("should damn well NOT ack.acknowledge", async () => {
         listener.returns(undefined);
-        return tq._listen(listener, msg, headers, info, ack).then(function() {
-          ack.acknowledge.should.not.have.been.called;
-          ack.retry.should.not.have.been.called;
-          return ack.fail.should.not.have.been.called;
-        });
+        await tq._listen(listener, msg, headers, info, ack);
+        ack.acknowledge.should.not.have.been.called;
+        ack.retry.should.not.have.been.called;
+        ack.fail.should.not.have.been.called;
       });
 
-      it("should however retry on fail", function() {
+      it("should however retry on fail", async () => {
         listener.throws(new Error("Bad bad"));
-        return tq._listen(listener, msg, headers, info, ack).then(function() {
-          ack.retry.should.have.been.called; //!
-          ack.acknowledge.should.not.have.been.calledOnce;
-          return ack.fail.should.not.have.been.called;
-        });
+        await tq._listen(listener, msg, headers, info, ack);
+        ack.retry.should.have.been.called; //!
+        ack.acknowledge.should.not.have.been.calledOnce;
+        ack.fail.should.not.have.been.called;
       });
     });
   });
 
-  describe(".subscribe()", function() {
+  describe(".subscribe()", () => {
     it("should use the default exchange", () => {
       amqpc.exchange.should.have.been.calledWith("test-flow", {
         autoDelete: true,
       });
     });
 
-    it("should set up a retry queue", () =>
-      tq.exchange.then(function() {
-        return amqpc.queue.should.have.been.calledWith("test-retries", {
-          durable: true,
-          autoDelete: false,
-          arguments: {
-            "x-message-ttl": 10000,
-            "x-dead-letter-exchange": "",
-            "x-dead-letter-routing-key": "test",
-          },
-        });
-      }));
+    it("should set up a retry queue", () => tq.exchange.then(function() {
+      return amqpc.queue.should.have.been.calledWith("test-retries", {
+        durable: true,
+        autoDelete: false,
+        arguments: {
+          "x-message-ttl": 10000,
+          "x-dead-letter-exchange": "",
+          "x-dead-letter-routing-key": "test",
+        },
+      });
+    }));
 
-    it("should set up a failures queue", () =>
-      tq.exchange.then(() =>
-        amqpc.queue.should.have.been.calledWith("test-failures", {
-          durable: true,
-          autoDelete: false,
-        })
+    it("should set up a failures queue", () => tq.exchange.then(() => amqpc.queue.should.have.been.calledWith("test-failures", {
+      durable: true,
+      autoDelete: false,
+    })
+    ));
+
+    it("should use default options if none are given", () => tq
+      .subscribe(function() { })
+      .then(() => queue.subscribe.should.have.been.calledWith(
+        { ack: true, prefetchCount: 1 },
+        match.func
+      )
       ));
 
-    it("should use default options if none are given", () =>
-      tq
-        .subscribe(function() { })
-        .then(() =>
-          queue.subscribe.should.have.been.calledWith(
-            { ack: true, prefetchCount: 1 },
-            match.func
-          )
-        ));
+    it("should use specified options, but set ack and prefetchCount", async () => {
+      await tq.subscribe(({ panda: "cub" } as amqp.SubscribeOpts), function() { });
+      queue.subscribe.should.have.been.calledWith(
+        { panda: "cub", ack: true, prefetchCount: 1 },
+        match.func
+      );
+    });
 
-    it("should use specified options, but set ack and prefetchCount", () =>
-      tq
-        .subscribe({ panda: "cub" } as amqp.SubscribeOpts, function() { })
-        .then(() =>
-          queue.subscribe.should.have.been.calledWith(
-            { panda: "cub", ack: true, prefetchCount: 1 },
-            match.func
-          )
-        ));
-
-    describe("should call subscribe on the underlying queue", function() {
-      beforeEach(function() {
+    describe("should call subscribe on the underlying queue", () => {
+      beforeEach(() => {
         queue.subscribe = spy();
         tq = new TenaciousQ(amqpc, queue);
       });
 
-      it("with a callback that in turn will invoke the listener", function(done) {
+      it("with a callback that in turn will invoke the listener", async (done) => {
         const listener = () => done();
-        return tq
-          .subscribe(listener)
-          .then(() =>
-            queue.subscribe.getCall(0).args[1]({}, {}, {}, { acknowledge() { } })
-          );
+        await tq.subscribe(listener);
+        queue.subscribe.getCall(0).args[1]({}, {}, {}, { acknowledge() { } });
       });
 
-      it("and when invoked, the listener should recieve an ack object", function(done) {
-        const listener = function(msg, headers, info, ack) {
+      it("and when invoked, the listener should recieve an ack object", (done) => {
+        const listener = (msg, headers, info, ack) => {
           try {
             ack.exchange.should.equal(exchange);
             // ack.should.be.instanceOf(Ack);
@@ -206,10 +200,9 @@ describe("TenaciousQ", function() {
             done(err);
           }
         };
-        tq.subscribe(listener).then(() =>
-          queue.subscribe
-            .getCall(0)
-            .args[1]("msg", "headers", "info", { acknowledge() { } })
+        tq.subscribe(listener).then(() => queue.subscribe
+          .getCall(0)
+          .args[1]("msg", "headers", "info", { acknowledge() { } })
         );
       });
     });
